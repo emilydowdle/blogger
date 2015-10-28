293d39d20efbf54ac27ab6af69cf95fc18fd01cb
Emily Dowdle <emily@editingemily.com>
2015-10-28 15:32:12 -0600

    completed comments functionality

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,30 @@
+class CommentsController < ApplicationController
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.article_id = params[:article_id]
+
+    @comment.save
+
+    redirect_to article_path(@comment.article)
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  private
+  
+  def comment_params
+    params.require(:comment).permit(:author_name, :body)
+  end
+
+end
